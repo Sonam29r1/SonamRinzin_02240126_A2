7754cc322960d05128961a444283166b8cad6628
Sonam29r1 <02240126.cst@rub.edu.bt>
2025-05-07 23:05:33 +0600

    changes made to menu

--- SonamRinzin_02240126_A2_PA.py
+++ SonamRinzin_02240126_A2_PA.py
@@ -1,50 +1,53 @@
-class Menu:
-    def __init__(self):
-        self.scores1 = 0
-        self.scores2 = 0
-        self.scores3 = 0
-        self.scores4 = 0
+class Main_menu:
     def menu(self):
-        while True:
-            print("Select a function (0-5)")
-            print("1. Guess the Number")
-            print("2. Rock Paper Scissors game")
-            print("3. Trivia Pursuit Game ")
-            print("4. Pokemon Card Binder Manager")
-            print("5. Check Current Overall Score")
-            print("0. Exit Program")
-    def program_run(self):
+        print("Select a function (0-5):")
+        print("1. Guess Number game")
+        print("2. Rock paper scissors game")
+        print("3. Trivia Pursuit Game")
+        print("4. Pokemon Card Binder Manager")
+        print("5. Check Current Overall score")
+        print("0. Exit program")
+        
+    def menu_run(self):
         while True:
             self.menu()
-            function_choice = int(input("Your choice: "))
-            if function_choice == 1 :
-                game = guess_the_number()
-                self.scores1 = game.play()
-            elif function_choice == 2 :
-                game = rock_paper_scissors()
-                self.scores2 = game.play()
-            elif function_choice == 3:
-                game = Trivia_pursuit_game()
-                self.scores3 = game.play()
-            elif function_choice == 4:
-                game = pokemon_card_binder_manager()
-                self.scores4 = game.play()
-            elif function_choice == 5:
-                slef.score_display()
-            elif function_choice == 0:
-                print("End")
+            player_choice = int(input("Enter the game you want to play: "))
+
+            if player_choice == 1:
+                #run guess the number game
+                print("okay")
+                print("--------------------------------------------------------------")
+            elif player_choice == 2:
+                #run rpc
+                print("")
+                print("--------------------------------------------------------------")
+                
+            elif player_choice == 3:
+                #run TPG
+                print("")
+                print("--------------------------------------------------------------")
+
+            elif player_choice == 4:
+                #run  PCBM
+                print("")
+                print("--------------------------------------------------------------")
+
+            elif player_choice == 5:
+                #Check scores
+                print("")
+                print("--------------------------------------------------------------")
+
+            elif player_choice == 0:
+                print("Exiting program !!")
+                print("--------------------------------------------------------------")
+
                 break
             else:
-                print("Invalid")  
-    def score_display():
-        print("Scores:")
-        print("Guess the number: "+ self.scores1 )  
-        print("Rock paper scissors: " + self.scores2)  
-        print("trivia quiz: "+ self.scores3)  
-        print("Pokemon binder:" + self.scores4)  
-        print("Total score:")  
-                
-#main execution of the game
-if __name__ = "__main__"
-    menu = Menu()
-    menu.run()
+                print("Invalid choice! please enter a valid choice")
+                print("--------------------------------------------------------------")
+
+
+
+if __name__ == "__main__":
+    menu = Main_menu()
+    menu.menu_run()
