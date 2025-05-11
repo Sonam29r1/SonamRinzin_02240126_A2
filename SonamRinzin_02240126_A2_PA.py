ee5a3d1138df659d412bfb0a5fbd88b51f2dd074
Sonam29r1 <02240126.cst@rub.edu.bt>
2025-05-11 19:03:32 +0600

    Finalized

--- SonamRinzin_02240126_A2_PA.py
+++ SonamRinzin_02240126_A2_PA.py
@@ -1,10 +1,16 @@
-from SonamRinzin_02240126_A2_PB import Pokemon_Binder_Manager
+from SonamRinzin_02240126_A2_PB import Pokemon_Binder_Manager #imports part b pokemon binder manager
+import random #for guess the number game and rock paper scissors
 
 class Main_menu:
-    def menu(self):
+    def __init__(self):
+        self.scoring = overall_scoring_system() #instantiating overall_scoring_system class in Main_menu
+
+    def menu(self): #list of fucntions to be displayed
+        print("★ ", "━"*40 ,"★")
         print("Select a function (0-5):")
+        print("★ ", "━"*40 ,"★")
         print("1. Guess Number game")
-        print("2. Rock paper scissors game")
+        print("2. Rock paper scissors game (✌️  ✊ ✋)")
         print("3. Trivia Pursuit Game")
         print("4. Pokemon Card Binder Manager")
         print("5. Check Current Overall score")
@@ -13,127 +19,145 @@ class Main_menu:
         
     def menu_run(self):
         while True:
-            self.menu()
-            player_choice = int(input("Enter the game you want to play: "))
-
-            if player_choice == 1:
-                #run guess the number game
-                game = Number_guessing()
-                game.game_execution()
-                score = game.game_execution()
-                self.scoring_system.update_score('guess_number', score)
-                print("--------------------------------------------------------------")
-            elif player_choice == 2:
-                #run rpc
-                game2 = Rock_paper_scissor()
-                game2.game_execution()
-                score = game2.game_execution()
-                self.scoring_system.update_score('rock_paper_scissors', score)
-                print("--------------------------------------------------------------")
+            self.menu() #displays the list of function(in a infinite loop)
+            print("★ ", "━"*40 ,"★")
+            player_choice = str(input("Enter the game you want to play: ")) #allows user to select a function
+            
+            if player_choice == "1":
+                #run guess the number game 
+                game = Number_guessing() #Number_guessing class is called and it is assigned to variable game
+                score = game.game_execution() #from Number_guessing class, function game-execution is called and it is assigned to variable score
+                self.scoring.score_updater('guess_number', score) #from overall_scoring_system class, score_updator function is called and score from the game is updated
+            elif player_choice == "2":
+                #run rock paper scissor game
+                game2 = Rock_paper_scissor() #Rock_paper_scissor class is called and it is assigned to variable game2
+                score = game2.game_execution() #from Rock_paper_scissor class, function game-execution is called and it is assigned to variable score
+                self.scoring.score_updater('rock_paper_scissors', score) ##from overall_scoring_system class, score_updator function is called and score from the game is updated
+                print("★ ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ★")
                 
-            elif player_choice == 3:
-                #run TPG
-                game3 = Trivia_quiz_game()
-                game3.quiz_execution()
-                score = game3.quiz_execution()
-                self.scoring_system.update_score('trivia_quiz', score)
-                print("--------------------------------------------------------------")
+            elif player_choice == "3":
+                #run Trivia quiz game
+                game3 = Trivia_quiz_game() #Trivia_quiz_game class is called and it is assigned to variable game
+                score = game3.quiz_execution() #from Trivia_quiz_game class, function quiz-execution is called and it is assigned to variable score
+                self.scoring.score_updater('trivia_quiz', score) #from overall_scoring_system class, score_updator function is called and score from the game is updated
+                print("★ ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ★")
 
-            elif player_choice == 4:
-                #run  PCBM
-                game4 = Pokemon_Binder()
-                game4.menu_run()
-                self.scoring_system.update_score('pokemon_binder', game4.manager.total_cards)
-                print("--------------------------------------------------------------")
+            elif player_choice == "4":
+                #run  Pokemon card binder manager
+                game4 = Pokemon_Binder() #Pokemon_Binder class is called and it is assigned to variable game
+                game4.menu_run() #from Pokemon_Binder class, function menu_run is called and list of functions is displayed
+                self.scoring.score_updater('pokemon_binder', game4.manager) #from overall_scoring_system class, score_updator function is called and score from the game is updated
+                print("★ ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ★")
 
-            elif player_choice == 5:
+            elif player_choice == "5":
                 #Check scores
-                self.scoring_system.display_scores()
-                print("--------------------------------------------------------------")
+                self.scoring.score_displayer() #from overall_scoring_system , score_displayer function is called and score is displayed
+                print("★ ", "━"*40 ,"★")
 
-            elif player_choice == 0:
+            elif player_choice == "0":
                 print("Exiting program !!")
-                print("--------------------------------------------------------------")
+                print("★ ", "━"*40 ,"★")
 
-                break
-            else:
+                break #exits the loop
+            else: 
                 print("Invalid choice! please enter a valid choice")
-                print("--------------------------------------------------------------")
+                print("★ ", "━"*40 ,"★")
 
 #creating class for each game
-import random
 class Number_guessing:
     def __init__(self):
         self.lowest_number = 0
-        self.highest_number = 40
+        self.highest_number = 9
     def game_execution(self):
-        number_to_be_guessed = random.randint(self.lowest_number,self.highest_number)
+        number_to_be_guessed = random.randint(self.lowest_number,self.highest_number) #A random number between 0 and 9 is selected
         attempt = 0
-        valid_numbers = 0
+        valid_numbers = 15
         correct_attempt = 0
         min = 0
         while True:
-            try:
-                print("I am thinking of a number between 0 and 40. \nCan you guess it?")
+            try: # used try to handle value error
+                print("I am thinking of a number between 0 and 15.\nCan you guess it?")
+                print("★ ", "━"*40 ,"★")
                 player_guess = int(input("Your guess is : "))
+                print("★ ", "━"*40 ,"★")
+
+                '''
+                Checks if the player's guess is correct or wrong. If correct congrats user and exit the loop.
+                If wrong notify if the guess the higher or lower than target number
+                keeps track of valid attempst,correct attempts and avoids counting invalid attempts
+                '''
                 if player_guess == number_to_be_guessed:
                     attempt += 1
-                    valid_numbers += 1
                     correct_attempt += 1
-                    print("Correct")
+                    print("Wow! Your a prophet!")
+                    print("★ ", "━"*40 ,"★")
                     break
                 elif player_guess < number_to_be_guessed:
                     attempt +=1
-                    print("Too low")
+                    print(f"Too bad {player_guess} is lower. Can you try higher")
+                    print("★ ", "━"*40 ,"★")
                     continue
                 elif player_guess > number_to_be_guessed:
                     attempt += 1
-                    print("Too high")
+                    print(f"Too bad {player_guess} is way higher. Can your try lower")
+                    print("★ ", "━"*20 ,"★")
                     continue
 
             except ValueError:
-                attempt +=1
-                print("Invalid input! \nEnter a valid input!!")
+                print("Invalid Guess! \nEnter a valid input!!")
                 continue
-        score = max(0,attempt-valid_numbers)
+        score = max(0,valid_numbers-attempt)
         print("Your score is ",score)
         return score
 
 class Rock_paper_scissor:
     def __init__(self):
-        self.choices = [1,2,3] #1 for rock, 2 for paper, 3 for scissors
+        self.choices = ["Rock","Paper","Scissor"] #creates a list of choice
         self.win = 0
         self.attempt = 0
     def game_execution(self):
         while True:
-            computer_choice = random.choice(self.choices)
+            computer_choice = random.choice(self.choices) #A random number from choices[1,2,3] is selected
             try:
-                player_choice = int(input("Enter: \n 1 for Rock \n 2 for Paper \n 3 for Scissors \n 4 to Quit \nYour choice is: "))
-                if player_choice == computer_choice:
-                    print("Its a tie")
-                    self.attempt += 1
+                '''
+                Used try and except for handling Value Error
+                used logical operator to determining the winner and loser.
+                Tracks attempt and wins
+                '''
+                print("★ ", "━"*40 ,"★")
+                player_choice = int(input("Enter Your Choice: \n Type 1 for Rock \n Type 2 for Paper \n Type 3 for Scissors \n Type 4 to Quit \nYour choice is: "))
+                if (player_choice ==1 and  computer_choice == "Rock") or \
+                   (player_choice == 2 and computer_choice == "Paper") or\
+                   (player_choice == 3 and computer_choice == "Scissor"): # same choice leads to tie
+                   print(f"Computer has chosen {computer_choice}! \nIts a tie")
+                   self.attempt += 1
 
-                elif(player_choice == 1 and computer_choice == 3) or \
-                    (player_choice == 2 and computer_choice == 1) or \
-                    (player_choice == 3 and computer_choice == 2):
-                    print("You win!")
+                elif(player_choice == 1 and computer_choice == "Scissor") or \
+                    (player_choice == 2 and computer_choice == "Rock") or \
+                    (player_choice == 3 and computer_choice == "Paper"): #This are all winning condotions for player
+                    print(f"You win! Computer has chosen {computer_choice}")
                     self.attempt += 1
                     self.win += 1
 
-                elif player_choice == 4:
+                elif player_choice == 4: #allows user to quit
+                    print("Thanks for playing")
                     break
                 else:
-                    print("You lost!")
+                    print(f"You lost! Computer has chosen {computer_choice}")
                     self.attempt +=1
-            except ValueError:
+            except ValueError: #handles Value Error
                 print("Invalid choice!")
                 continue
-        print(f"You won {self.win} time against computer in {self.attempt} match")
+        print(f"You won {self.win} time against computer in {self.attempt} match") #Displays the score(number of wins against computer)
+        print("★ ", "━"*20 ,"★")
         return self.win
                     
 class Trivia_quiz_game:
     def __init__(self):
         self.score = 0
+        '''
+        Creates a nested dict which includes category{question,option and answer}
+        '''
         self.questions = {
             'Kings of Bhutan' : [
                 {
@@ -177,37 +201,47 @@ class Trivia_quiz_game:
     def quiz_execution(self):
         print("Welcome to Trivia Pursuit Game!")
         print("Select a category:")
-        categories = list(self.questions.keys())
-        for i in range(0,len(categories)):
-            print(f"{i}. {categories[i]}")
+        print("-"*44)
+
+        categories = list(self.questions.keys()) #A list is created from above dict using its keys(category) 
+        for i in range(0,len(categories)): #Prints all the keys(categories) in the dict which is stored as list
+            print(f"{i+1}. {categories[i]}")
+            print("-"*44)
             
         try:
-            category_choice = int(input("Enter category number: ")) - 1
+            category_choice = int(input("Enter category number: ")) - 1 #lets the user select a category(-1 because index starts from 0 and category choice starts from 1)
             selected_category = categories[category_choice]
-        except (ValueError, IndexError):
+        except (ValueError, IndexError): #handle error
             print("Invalid category selection.")
-            return 0
+            print("★ ", "━"*20 ,"★")
+            return 
             
-        for question in self.questions[selected_category]:
-            print("\n" + question['question'])
-            for option in question['options']:
+        for question in self.questions[selected_category]: # this let us access every question for our selected category
+            print(question['question'])
+            print("★ ", "━"*20 ,"★")
+            for option in question['options']: #prints the option of corresponding question listed in the dict
                 print(option)
+            print("★ ", "━"*20 ,"★")
                 
-            user_answer = input("Your answer (A/B/C/D): ").upper()
-            if user_answer == question['answer']:
+            user_answer = input("Your answer (A/B/C/D): ").upper() #lets user enter a choice and make it case insensitive
+            if user_answer == question['answer']: # uses conditional operation to check if user's answer matches the answer in the dict
                 print("Correct!")
-                self.score += 1
+                print("★ ", "━"*20 ,"★")
+                self.score += 1 #if correct answer adds +1 to score
             else:
                 print(f"Wrong! The correct answer is {question['answer']}")
+                print("★ ", "━"*20 ,"★")
                 
         print(f"\nYour score: {self.score}/{len(self.questions[selected_category])}")
-        return self.score
+        return self.score #returns score value to be used in overall score
     
 
 class Pokemon_Binder:
     def __init__(self):
-        self.manager = Pokemon_Binder_Manager()
-    def menu(self):
+        self.manager = Pokemon_Binder_Manager() #initializes the manager attribute as an instance of Pokemon_Binder_Manager() class
+    def menu(self): #game menu to be displayed
+        print("Welcome to Pokemon Card Binder Manager")
+        print("★ ", "━"*20 ,"★")
         print("Game Menu:")
         print("1. Add Pokemon Card")
         print("2. Reset Binder")
@@ -216,23 +250,27 @@ class Pokemon_Binder:
     
     def menu_run(self):
         while True:
-            self.menu()
+            self.menu() #displays the game menu
             try:
-                player_choice = int(input("Enter your choice:"))
+                player_choice = int(input("Enter your choice:")) #lets user select a function
                 if player_choice == 1 :
                     try:
-                        user_choice = int(input("Enter Pokedex Number (1-1025): "))
-                        self.manager.add_card(user_choice)
-                    except ValueError:
+                        user_choice = int(input("Enter Pokedex Number (1-1025): ")) #Lets user enter pokedex number
+                        self.manager.add_card(user_choice) #from part b, uses add_card function of pokemon_binder_manager class and a card is added as the user_choice input is passed down as a parameter for add_card function
+                        ("★ ", "━"*20 ,"★")
+                    except ValueError: #handles ValueError 
                         print("Invalid Input! Please enter a number.")
+                        ("★ ", "━"*20 ,"★")
                 elif player_choice == 2:
-                    self.manager.reset_binder()
+                    self.manager.reset_binder() #uses reset_binder function from pokemon_binder_manager class in part b.
+                    ("★ ", "━"*20 ,"★")
                 elif player_choice == 3:
-                    self.manager.display_binder()
+                    self.manager.display_binder() #uses display_binder function from pokemon_binder_manager class in part b
+                    ("★ ", "━"*20 ,"★")
                     
                 elif player_choice == 4:
                     print("Exiting")
-                    break
+                    break #lets player exit by breaking the loop
                 else:
                     print("Invalid")
             except ValueError:
@@ -250,16 +288,21 @@ class overall_scoring_system:
             self.overall_score[game_name] = score
     def score_displayer(self):
         print("Your Current Overall Score is: ")
+        '''
+        Displays the corresponding values of each given key which is stored as the score of each game
+        '''
+        print("★ ", "━"*20 ,"★")
         print(f"1. Guess the Number Game: {self.overall_score['guess_number']}")
-        print(f"2. Rock Paper Scissors: {self.overall_score['rock_paper_scissors']}")
+        print(f"2. Rock Paper Scissors: {self.overall_score['rock_paper_scissors']} ")
         print(f"3. Trivia Quiz Game: {self.overall_score['trivia_quiz']}")
         print(f"4. Pokemon Binder: {self.overall_score['pokemon_binder']}")
+
 
 
             
 
     
 
-if __name__ == "__main__":
-    menu = Main_menu()
-    menu.menu_run()
+if __name__ == "__main__": #this make our code run only when we execute the file directly
+    menu = Main_menu() #creates the main_menu obj
+    menu.menu_run() #starts the program
--- SonamRinzin_02240126_A2_PB.py
+++ SonamRinzin_02240126_A2_PB.py
@@ -1,62 +1,92 @@
 
 class Pokemon_Binder_Manager:
     def __init__(self):
-        self.binder = {}  # Dictionary to store card info
-        self.total_cards = 0
-        self.max_pokedex = 1025
+        self.binder = {}  # used dictionary to store card info since it overwrites exisiting keys or vlaues(suitable as it avoids duplicates)
+        self.total_cards = 0 # 
+        self.max_pokedex = 1025 # Limit is 1025
         self.cards_per_page = 64  # 8x8 grid
-        self.rows_per_page = 8
-        self.cols_per_page = 8
+        self.rows_per_page = 8 # 8 rows per page
+        self.cols_per_page = 8 # 8 rows per colum
 
-    def calculate_position(self, pokedex_num):  # Fixed method name
-        pos_in_collection = pokedex_num - 1  # Convert to 0-based index
-        page = (pos_in_collection // self.cards_per_page) + 1
-        pos_in_page = pos_in_collection % self.cards_per_page
+    def calculate_position(self, pokedex_num):
+        """
+        Algorithm and logic:
+        1. First, we convert the Pokedex number to a 0-based index to avoid the need to handle page 0
+        2. Then, to determin the page number, used floor division to find which page the card belongs to by using the formula pokedex Number//64 and add 1 to turn into 1_based index since our card starts from 1 to 1025
+        - since a page hold 64 cards and we need an whole number as a page number.
+        - if we take 2 as our pokdex number we get 2//64 = 0 then add 1 and we get our page number as 1
+        - if pokedex no = 132 we get 132//64 = 2 and then add one and we get our page as 3
+        3. To determine the position within the page, used modulo operation since the remainder is the postion in page and we add one since our pokedex number is 0 based index(turns to 1_based index)
+        - if we take 2 as position_in_collection, 2 % 64 = 0(adds 1 when calculationg row and column)
+        - if position_in_collection = 132, 132 % 64 = 4(add 1 in row and column)
+        4. Calculate the row and column within the page by further division and modulo operations.(Same logic applies here)
+        - we take pos_in_page for calculationg row and column since this shows where the card's slot is located in the page from 0 to 63
+        - then we use (pos_in_page // 8 (since is 8x8) and adds 1 since our binder is 1-based index
+        - for column we use (pos_in _page // 8(8x8) ) and adds 1
+        """
+        position_in_collection = pokedex_num - 1  # Convert to 0-based index
+        page = (position_in_collection // self.cards_per_page) + 1 
+        pos_in_page = position_in_collection % self.cards_per_page
         row = (pos_in_page // self.cols_per_page) + 1
-        col = (pos_in_page % self.cols_per_page) + 1
-        return {'page': page, 'row': row, 'col': col}    
+        column = (pos_in_page % self.cols_per_page) + 1
+        return {'page': page, 'row': row, 'col': column}    
     
     def add_card(self, pokedex_num):
-        # Validation checks
-        if not 1 <= pokedex_num <= 1025:
+        '''
+        Validation
+        - entered card number should be between 1 and 1025
+        '''
+        if not 1 <= pokedex_num <= 1025: # Handles all other invalid inputs
             print("❌ Error: Invalid Pokedex number 0. Must be between 1-1025")
             return
-        if pokedex_num in self.binder:
+        
+        '''
+        Avoids(overwrites) duplicated cards in the binder
+        '''
+        if pokedex_num in self.binder: #checks if card exist in the binder
             print(f"⚠️ Card #{pokedex_num} already exists in the binder!")
             return
         
+        '''
+        Entered pokedex number(pokedex_num) is taken and uses calculate_postion funtion to calculate its position
+        in the binder
+        '''
         position = self.calculate_position(pokedex_num)
         self.binder[pokedex_num] = position
         self.total_cards += 1
         
-        # Print placement info
+        '''Above calculated postion is printed or displayed'''
         print(f"Page: {position['page']}")
         print(f"Position: Row {position['row']}, Column {position['col']}")
         print(f"Status: Added Pokedex #{pokedex_num}")
         
-        # Check for complete collection
+        # Checks if card collection is completed 
         if self.total_cards == self.max_pokedex:
-            print("🎉 CONGRATULATIONS! You've completed the entire collection! 🎉")
+            print("🎉 CONGRATULATIONS! You've caught them all! 🎉")
             
+    
     def reset_binder(self):
         print("WARNING: This will delete ALL Pokemon cards from the binder")
-        confirmation = input("Type 'CONFIRM' to reset: ").upper()
+        confirmation = input("Type 'CONFIRM' to reset or any other key to CANCEL: ").upper() #Confirmation from players and made case insenstive
+
+        '''If user confrims to reset, uses dict method clear to erase everthing'''
         
-        if confirmation == "CONFIRM":
-            self.binder.clear()
-            self.total_cards = 0
+        if confirmation == "CONFIRM": #User confrimation to avoid accidental data loss
+            self.binder.clear() #upon confrimation clears the binder
+            self.total_cards = 0 # set total cards value to 0
             print("Binder reset successfully")
         else:
             print("Reset cancelled")
 
     def display_binder(self):
         print("\nCurrent Binder Contents:")
+        '''For every pokedex number in our sorted binder, we print its pokdex num along with its postion'''
         for pokedex_num in sorted(self.binder.keys()):
             pos = self.binder[pokedex_num]
-            print(f"Pokedex #{pokedex_num}: Page {pos['page']}, Row {pos['row']}, Column {pos['col']}")
+            print(f"Pokedex #{pokedex_num} \n Page :{pos['page']}, Row :{pos['row']}, Column:{pos['col']}")
         
-        print(f"\nTotal cards: {self.total_cards}")
-        print(f"Completion: {(self.total_cards/self.max_pokedex)*100:.2f}%")
+        print(f"\nTotal cards: {self.total_cards}") #Displays how many cards player have collected so far
+        print(f"Completion: {(self.total_cards/self.max_pokedex)*100:.2f}%") # calculates and displays the percentage of card collection and round it to two decimals
     
 
 
